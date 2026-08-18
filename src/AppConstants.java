ef1b21066dadeeb4c2d1a660fa82cb880fa11546
Anthony <akhat711@gmail.com>
2026-08-18 22:42:54 +1000

    added buttons, gitignore, and touched files

--- src/AppConstants.java
+++ src/AppConstants.java
@@ -1,5 +1,8 @@
+import java.awt.*;
+
 /// Constants such as window size, fonts, colors, should be placed here to maintain readability.
 public interface AppConstants {
     int SCREEN_WIDTH = 800;
     int SCREEN_HEIGHT = 600;
+    Font DEFAULT_FONT = new Font("Arial", Font.BOLD, 16);
 }
