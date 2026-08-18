6a25286c1f70be9462d0bdab5fa4f606c0f9ded6
Anthony <akhat711@gmail.com>
2026-08-18 14:52:45 +1000

    Added GUI class. imported libraries. Added constants class for readibility.

---
+++ src/AppConstants.java
@@ -0,0 +1,5 @@
+/// Constants such as window size, fonts, colors, should be placed here to maintain readability.
+public interface AppConstants {
+    int SCREEN_WIDTH = 800;
+    int SCREEN_HEIGHT = 600;
+}
